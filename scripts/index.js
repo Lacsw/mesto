12d74bf58d44a41f2690988bdb132fb9c2c1b358
Roman Frolov <frolloff1@yandex.ru>
2022-10-21 17:12:45 +0300

    feat: добавить возможность добавления карточки/лайк/удаление

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,120 @@
+const POPUP_OPENED_CLASS = 'popup_opened';
+
+const popupTypeEdit = document.querySelector('.popup_type_edit');
+const popupTypeAdd = document.querySelector('.popup_type_add');
+const closeButtons = document.querySelectorAll('.popup__close-btn');
+const inputPlaceName = document.querySelector('.popup__input_type_place-name');
+const inputPlaceLink = document.querySelector('.popup__input_type_link');
+const inputProfileName = document.querySelector('.popup__input_type_name');
+const inputProfileJob = document.querySelector('.popup__input_type_job');
+const formEditProfile = document.querySelector('.popup__edit-form');
+const formAddCard = document.querySelector('.popup__add-form');
+
 let profileEditBtn = document.querySelector('.profile__edit');
-let popup = document.querySelector('.popup');
-let popupCloseBtn = document.querySelector('.popup__close-btn');
-let inputName = document.querySelector('.popup__input_type_name');
-let inputJob = document.querySelector('.popup__input_type_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let formElement = document.querySelector('.popup__edit-form');
+let profileAddBtn = document.querySelector('.profile__add-btn');
 
-function openPopup() {
-  popup.classList.add('popup_opened');
+const cardsContainer = document.querySelector('.cards');
+
+const initialCards = [
+  {
+    name: 'Северо-Атлантический океан',
+    link: './images/blake-cheek.jpeg',
+  },
+  {
+    name: 'El Matador',
+    link: './images/el-matador-beach.jpeg',
+  },
+  {
+    name: 'Мадасари',
+    link: './images/pantai-mandasari.jpeg',
+  },
+  {
+    name: 'Калифорния',
+    link: './images/california.jpeg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+  {
+    name: 'Страдброк',
+    link: './images/stradbroke-island.jpeg',
+  },
+];
+
+function createCard(cardName, cardLink) {
+  const cardTemplate = document.querySelector('.card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+
+  cardElement.querySelector('.card__title').textContent = cardName;
+  cardImage.src = cardLink;
+  cardImage.alt = cardName;
+
+  cardElement
+    .querySelector('.card__like-btn')
+    .addEventListener('click', (evt) => evt.target.classList.toggle('card__like-btn_active'));
+
+  cardElement
+    .querySelector('.card__remove-btn')
+    .addEventListener('click', () => cardElement.remove());
+
+  return cardElement;
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function addCard(cardName, cardLink) {
+  cardsContainer.prepend(createCard(cardName, cardLink));
 }
 
-function handleOpenProfilePopup () {
-  openPopup();
-  inputName.value = profileName.textContent;
-  inputJob.value = profileJob.textContent;
+function renderCards() {
+  initialCards.forEach((item) => {
+    addCard(item.name, item.link);
+  });
+}
+renderCards();
+
+function openPopup(popup) {
+  popup.classList.add(POPUP_OPENED_CLASS);
 }
 
-function formSubmitHandler(evt) {
+function closePopup(popup) {
+  popup.classList.remove(POPUP_OPENED_CLASS);
+}
+
+closeButtons.forEach((button) => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(popup));
+});
+
+function handleOpenProfilePopup() {
+  inputProfileName.value = profileName.textContent;
+  inputProfileJob.value = profileJob.textContent;
+  openPopup(popupTypeEdit);
+}
+
+function formEditProfileSubmitHandler(evt) {
   evt.preventDefault();
-  let newName = inputName.value;
-  let newJob = inputJob.value;
+  let newName = inputProfileName.value;
+  let newJob = inputProfileJob.value;
   profileName.textContent = newName;
   profileJob.textContent = newJob;
-  closePopup();
+  closePopup(popupTypeEdit);
 }
 
-profileEditBtn.addEventListener('click',  handleOpenProfilePopup);
-popupCloseBtn.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+function handleOpenAddCardPopup() {
+  openPopup(popupTypeAdd);
+}
+
+function formAddCardSubmitHandler(evt) {
+  evt.preventDefault();
+  addCard(inputPlaceName.value, inputPlaceLink.value);
+  closePopup(popupTypeAdd);
+  evt.target.reset();
+}
+
+profileEditBtn.addEventListener('click', handleOpenProfilePopup);
+formEditProfile.addEventListener('submit', formEditProfileSubmitHandler);
+profileAddBtn.addEventListener('click', handleOpenAddCardPopup);
+formAddCard.addEventListener('submit', formAddCardSubmitHandler);

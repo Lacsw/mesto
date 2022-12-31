4cbd316ff664850a5b23d92918df938bb4e233ac
Roman Frolov <frolloff1@yandex.ru>
2022-12-31 14:38:40 +0300

    ref: создать функию удаления слушателей событий для Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,7 +2,7 @@ export class Popup {
   _popupElement;
   _popupSelector;
 
-  constructor({popupSelector}) {
+  constructor({ popupSelector }) {
     this._popupSelector = popupSelector;
     this._popupElement = document.querySelector(this._popupSelector);
   }
@@ -24,14 +24,18 @@ export class Popup {
     document.addEventListener('keydown', this._handleEscClose);
   }
 
+  _removeEventListeners() {
+    this._popupElement.removeEventListener('click', this._hendleClickClose);
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
+
   open() {
     this._setEventListeners();
     this._popupElement.classList.add('popup_opened');
   }
 
   close() {
+    this._removeEventListeners();
     this._popupElement.classList.remove('popup_opened');
-    this._popupElement.removeEventListener('click', this._hendleClickClose);
-    document.removeEventListener('keydown', this._handleEscClose);
   }
 }
